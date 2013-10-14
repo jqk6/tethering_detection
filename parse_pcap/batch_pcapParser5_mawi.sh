0afd94fcb77e730f6daf20fee14ccee31c109856
yichao0319 <yichao0319@gmail.com>
2013-10-14 10:44:15 -0500

    take pcap parsing to independent task

--- parse_pcap/batch_pcapParser5_mawi.sh
+++ parse_pcap/batch_pcapParser5_mawi.sh
@@ -9,6 +9,6 @@ files=`ls $dir/pcap/*dump | xargs -n1 basename`
 for f in $files ; do
    date
    echo "  "$f
-   /export/home/ychen/testbed/subtask_tcp_timestamp/find_tcp_timestamp $dir/pcap/$f > $dir/text5/$f.txt
+   ./pcapParser5 $dir/pcap/$f > $dir/text5/$f.txt
 done
 
