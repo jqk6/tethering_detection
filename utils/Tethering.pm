99df603c182aad0b847b5a533846a243fa46487b
yichao0319 <yichao0319@gmail.com>
2013-10-30 18:21:29 -0700

    add new method

--- utils/Tethering.pm
+++ utils/Tethering.pm
@@ -603,29 +603,29 @@ sub calculate_flow_frequency_enumeration_boot_span {
 ## @output freq      : -1 if cannot find a frequency which make the differenc of boot times smaller than the threshold
 ## @output boot_time : the average boot time if freq != -1
 ##
-sub est_freq_boottime_enumeration1 {
-    my ($rx_time_ref, $tx_ts_ref) = @_;
+# sub est_freq_boottime_enumeration1 {
+#     my ($rx_time_ref, $tx_ts_ref) = @_;
     
-    my $boot_span_threshold = 3;
-    my $gr_bt_type = 0;
-    my $flow_pktnum_threshold = 30;
-    my $flow_dur_threshold = 5;
-    my @freqs = (2, 10, 100, 128, 200, 1000);
+#     my $boot_span_threshold = 3;
+#     my $gr_bt_type = 0;
+#     my $flow_pktnum_threshold = 30;
+#     my $flow_dur_threshold = 5;
+#     my @freqs = (2, 10, 100, 128, 200, 1000);
 
-    return _est_freq_boottime_enumeration($rx_time_ref, $tx_ts_ref, \@freqs, $boot_span_threshold, $flow_pktnum_threshold, $flow_dur_threshold);
-}
+#     return _est_freq_boottime_enumeration($rx_time_ref, $tx_ts_ref, \@freqs, $boot_span_threshold, $flow_pktnum_threshold, $flow_dur_threshold);
+# }
 
-sub est_freq_boottime_enumeration2 {
-    my ($rx_time_ref, $tx_ts_ref) = @_;
+# sub est_freq_boottime_enumeration2 {
+#     my ($rx_time_ref, $tx_ts_ref) = @_;
     
-    my $boot_span_threshold = 3;
-    my $gr_bt_type = 0;
-    my $flow_pktnum_threshold = 30;
-    my $flow_dur_threshold = 5;
-    my @freqs = (1 .. 1000);
+#     my $boot_span_threshold = 3;
+#     my $gr_bt_type = 0;
+#     my $flow_pktnum_threshold = 30;
+#     my $flow_dur_threshold = 5;
+#     my @freqs = (1 .. 1000);
 
-    return _est_freq_boottime_enumeration($rx_time_ref, $tx_ts_ref, \@freqs, $boot_span_threshold, $flow_pktnum_threshold, $flow_dur_threshold);
-}
+#     return _est_freq_boottime_enumeration($rx_time_ref, $tx_ts_ref, \@freqs, $boot_span_threshold, $flow_pktnum_threshold, $flow_dur_threshold);
+# }
 
 #######
 ## _est_freq_boottime_enumeration
@@ -642,54 +642,54 @@ sub est_freq_boottime_enumeration2 {
 ## @output freq      : -1 if cannot find a frequency which make the differenc of boot times smaller than the threshold
 ## @output boot_time : the average boot time if freq != -1
 ##
-sub _est_freq_boottime_enumeration {
-    my ($rx_time_ref, $tx_ts_ref, $freqs_ref, $gr_bt_type, $boot_span_threshold, $flow_pktnum_threshold, $flow_dur_threshold) = @_;
+# sub _est_freq_boottime_enumeration {
+#     my ($rx_time_ref, $tx_ts_ref, $freqs_ref, $gr_bt_type, $boot_span_threshold, $flow_pktnum_threshold, $flow_dur_threshold) = @_;
     
-    die "tx and rx array size do not match\n" if(scalar(@$tx_ts_ref) != scalar(@$rx_time_ref));
+#     die "tx and rx array size do not match\n" if(scalar(@$tx_ts_ref) != scalar(@$rx_time_ref));
 
-    ## check flow pkt num
-    return (-1, -1) if(scalar(@$rx_time_ref) < $flow_pktnum_threshold);
-    ## check flow duration
-    return (-1, -1) if(($rx_time_ref->[-1] - $rx_time_ref->[0]) < $flow_dur_threshold);
+#     ## check flow pkt num
+#     return (-1, -1) if(scalar(@$rx_time_ref) < $flow_pktnum_threshold);
+#     ## check flow duration
+#     return (-1, -1) if(($rx_time_ref->[-1] - $rx_time_ref->[0]) < $flow_dur_threshold);
 
 
-    my $min_span = $boot_span_threshold + 1;
-    my $min_span_freq = 0;
-    my $min_span_boot_time = 0;
-    my $group_boot_time = -1;
-    foreach my $this_freq (@$freqs_ref) {
-        my @this_boot_times = ();
-        foreach my $ind (0 .. scalar(@$tx_ts_ref)-1) {
-            push(@this_boot_times, $rx_time_ref->[$ind] - $tx_ts_ref->[$ind] / $this_freq);
-        }
+#     my $min_span = $boot_span_threshold + 1;
+#     my $min_span_freq = 0;
+#     my $min_span_boot_time = 0;
+#     my $group_boot_time = -1;
+#     foreach my $this_freq (@$freqs_ref) {
+#         my @this_boot_times = ();
+#         foreach my $ind (0 .. scalar(@$tx_ts_ref)-1) {
+#             push(@this_boot_times, $rx_time_ref->[$ind] - $tx_ts_ref->[$ind] / $this_freq);
+#         }
 
-        my $this_boot_time_span = abs( max(@this_boot_times) - min(@this_boot_times) );
-        if($this_boot_time_span < $min_span) {
-            $min_span = $this_boot_time_span;
-            $min_span_freq = $this_freq;
-            $min_span_boot_time = MyUtil::average(\@this_boot_times);
-        }
-        if($this_freq == 100 && $gr_bt_type == 1) {
-            $group_boot_time = MyUtil::average(\@this_boot_times);
-        }
-        else {
-            $group_boot_time = $min_span_boot_time;
-        }
-    }
-    if($group_boot_time < 0 && $gr_bt_type == 1) {
-        my @this_boot_times = ();
-        foreach my $ind (0 .. scalar(@$tx_ts_ref)-1) {
-            push(@this_boot_times, $rx_time_ref->[$ind] - $tx_ts_ref->[$ind] / 100);
-        }
-        $group_boot_time = MyUtil::average(\@this_boot_times);
-    }
-    if($min_span < $boot_span_threshold){
-        return ($min_span_freq, $group_boot_time);
-    }
-    else {
-        return (-1, -1);
-    }
-}
+#         my $this_boot_time_span = abs( max(@this_boot_times) - min(@this_boot_times) );
+#         if($this_boot_time_span < $min_span) {
+#             $min_span = $this_boot_time_span;
+#             $min_span_freq = $this_freq;
+#             $min_span_boot_time = MyUtil::average(\@this_boot_times);
+#         }
+#         if($this_freq == 100 && $gr_bt_type == 1) {
+#             $group_boot_time = MyUtil::average(\@this_boot_times);
+#         }
+#         else {
+#             $group_boot_time = $min_span_boot_time;
+#         }
+#     }
+#     if($group_boot_time < 0 && $gr_bt_type == 1) {
+#         my @this_boot_times = ();
+#         foreach my $ind (0 .. scalar(@$tx_ts_ref)-1) {
+#             push(@this_boot_times, $rx_time_ref->[$ind] - $tx_ts_ref->[$ind] / 100);
+#         }
+#         $group_boot_time = MyUtil::average(\@this_boot_times);
+#     }
+#     if($min_span < $boot_span_threshold){
+#         return ($min_span_freq, $group_boot_time);
+#     }
+#     else {
+#         return (-1, -1);
+#     }
+# }
 
 
 ########################################################
@@ -747,11 +747,11 @@ sub _identify_os {
 
 
 ########################################################
-## Timestamp Continuity Heuristic
+## Timestamp Monotonicity Heuristic
 ########################################################
 
 #######
-## check_timestamp_continuity
+## check_timestamp_monotonicity
 ##   check if the timestamps are continuous number
 ##
 ## @input rx_time_ref           : the array reference of the receiving times
@@ -763,7 +763,7 @@ sub _identify_os {
 ##
 ## @output continuity           : 0: Timestamp are not continuous; 1: otherwise
 ##
-sub check_timestamp_continuity {
+sub check_timestamp_monotonicity {
     my ($rx_time_ref, $tx_ts_ref, $freq_thresh, 
         $tolerate_wrap, $tolerate_disorder, $tolerate_gap) = @_;
     
@@ -887,4 +887,19 @@ sub check_gap_ttl_num {
 }
 
 
+########################################################
+## Frequency Stability
+## not a detection method, just to check if the frequency is stable
+## 1. check_freq_stability_enu
+##    calculate frequency and see if it is far away from known freq
+## 2. check_freq_stability_stdev
+##    calculate frequency using each packet and see if the stdev is larger than a threshold
+########################################################
+
+
+
+########################################################
+## IP ID
+## not a detection method, just to check if IP ID monotonicity
+########################################################
 1;
